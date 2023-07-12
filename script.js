90855a71705f91987cb27693206f72699ff9854e
aanchalsonu <aanchalsuntwal@gmail.com>
2023-07-12 20:20:04 +0530

    Added Comments with code analysis

--- script.js
+++ script.js
@@ -2,38 +2,17 @@ const main = document.getElementById('main')
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3aa79bcaccfa3a15f30191a9baf734ad&page=1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w500'
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3aa79bcaccfa3a15f30191a9baf734ad&query="'
-const open_btn = document.querySelector('.open-btn')
-const close_btn = document.querySelector('.close-btn')
-const nav = document.querySelectorAll('.nav')
-const form = document.getElementById('form')
+
 const search = document.getElementById('search')
 getMovies(API_URL)
 
-// open_btn.addEventListener('click', ()=>{
-//     nav.forEach(nav_el => nav_el.classList.add('visible'))
-// }
-// )
-// function search(query) {
-//     if (query.slice(0, 7) == "http://") {
-//       // window.location.href = query
-//       window.open(query, '_blank');
-//     } else {
-//       // window.location.href = "https://www.google.com/search?q=" + query
-//       debugger;
-//       window.open("https://www.google.com/search?q=" + query, '_blank');
-//     }
-// }
-
-// close_btn.addEventListener('click', ()=>{
-//     nav.forEach(nav_el => nav_el.classList.remove('visible'))
-// }
-// )
-
-
 async function getMovies(url){
-    const res = await fetch(url)
+    // Send request to the endpoint and await is to stop 
+    //execution until the promise is resolved.
+    const res = await fetch(url) // fetch() will fetch the data
     const data = await res.json()
-    
+    //Convert the received data into json format
+    //send the data to the showMovies function where it will be displayed
     showmovies(data.results)
 }
 
@@ -42,13 +21,15 @@ function showmovies(movies){
 
     movies.forEach((movie) =>{
         const { title, poster_path , vote_average, overview } = movie
-
+        //The above statement is Object destructuring , where the data from movies is assisgned to 
+        //the variables
         const movieEl = document.createElement('div')
         movieEl.classList.add('movie')
         movieEl.innerHTML = ` 
         <img src="${IMG_PATH + poster_path}" alt="${title}">
         <div class="movieinfo">
             <h3>${title}</h3>
+            // Here to get the color on the rating we call function getClassbyRate
             <span class="${getclassbyrate(vote_average)}">${vote_average}</span>
         </div>
         <div class="overview">
@@ -77,7 +58,7 @@ function getclassbyrate(vote)
 
 form.addEventListener('submit',(e) =>{
     e.preventDefault()
-
+    //to prevent default form submission
     const searchterm = search.value
     if(searchterm ){
         getMovies(SEARCH_API + searchterm)
